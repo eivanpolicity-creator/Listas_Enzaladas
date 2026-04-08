4e2dd12a495eb09513b51dbd4784110e248cbabb
eivanpolicity-creator <eivanpolicity@gmail.com>
2026-04-08 16:17:18 +0000

    Visible

--- Node.py
+++ Node.py
@@ -72,35 +72,31 @@ class LinkedList:
             curr = next_node
         self.head = prev
 
-        def delete_first(self):
-        if self.head:
-            self.head = self.head.next
-
-    def delete_by_value(self, value):
-        if not self.head: return
-        if self.head.data == value:
-            self.head = self.head.next
+        def sort_by_links(self):
+        if not self.head or not self.head.next:
             return
-        curr = self.head
-        while curr.next and curr.next.data != value:
-            curr = curr.next
-        if curr.next:
-            curr.next = curr.next.next
+        
+        changed = True
+        while changed:
+            changed = False
+            prev = None
+            curr = self.head
+            while curr and curr.next:
+                if curr.data > curr.next.data:
+                    
+                    temp = curr.next
+                    curr.next = temp.next
+                    temp.next = curr
+                    
+                    if prev is None:
+                        self.head = temp
+                    else:
+                        prev.next = temp
+                    
+                    changed = True
+                    prev = temp 
+                else:
+                    prev = curr
+                    curr = curr.next
 
-    def get_size(self):
-        count = 0
-        curr = self.head
-        while curr:
-            count += 1
-            curr = curr.next
-        return count
 
-    def reverse(self):
-        prev = None
-        curr = self.head
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-        self.head = prev
